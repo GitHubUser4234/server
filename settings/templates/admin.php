owncloud/core

Add option to disable autocomplete in share dialog

settings/templates/admin.php
@@ -245,6 +245,11 @@ if ($_['cronErrors']) {
 			<br />
 			<em><?php p($l->t('These groups will still be able to receive shares, but not to initiate them.')); ?></em>
 		</p>
+		<p class="<?php if ($_['shareAPIEnabled'] === 'no') p('hidden');?>">
+			<input type="checkbox" name="shareapi_allow_share_dialog_user_enumeration" value="1" id="shareapi_allow_share_dialog_user_enumeration"
+				<?php if ($_['allowShareDialogUserEnumeration'] === 'yes') print_unescaped('checked="checked"'); ?> />
+			<label for="shareapi_allow_share_dialog_user_enumeration"><?php p($l->t('Allow username autocomplete in share dialog. If this is disabled the full username needs to be entered.'));?></label><br />
+		</p>
 
 		<?php print_unescaped($_['fileSharingSettings']); ?>
 	</div>

GitHubUser4234/server

settings/templates/admin.php
@@ -265,6 +265,11 @@ if ($_['cronErrors']) {
 			<br />
 			<em><?php p($l->t('These groups will still be able to receive shares, but not to initiate them.')); ?></em>
 		</p>
+		<p class="<?php if ($_['shareAPIEnabled'] === 'no') p('hidden');?>">
+			<input type="checkbox" name="shareapi_allow_share_dialog_user_enumeration" value="1" id="shareapi_allow_share_dialog_user_enumeration"
+				<?php if ($_['allowShareDialogUserEnumeration'] === 'yes') print_unescaped('checked="checked"'); ?> />
+			<label for="shareapi_allow_share_dialog_user_enumeration"><?php p($l->t('Allow username autocompletion in share dialog. If this is disabled the full username needs to be entered.'));?></label><br />
+		</p>
 
 		<?php print_unescaped($_['fileSharingSettings']); ?>
 	</div>