switch-ch/core

Consolidate webdav code - move all to one app

apps/dav/appinfo/v1/publicwebdav.php
@@ -1,0 +1,73 @@
+<?php
+/**
+ * @author Lukas Reschke <lukas@owncloud.com>
+ * @author Morris Jobke <hey@morrisjobke.de>
+ * @author Robin Appelman <icewind@owncloud.com>
+ * @author Thomas Müller <thomas.mueller@tmit.eu>
+ * @author Vincent Petry <pvince81@owncloud.com>
+ *
+ * @copyright Copyright (c) 2015, ownCloud, Inc.
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+// load needed apps
+$RUNTIME_APPTYPES = ['filesystem', 'authentication', 'logging'];
+
+OC_App::loadApps($RUNTIME_APPTYPES);
+
+OC_Util::obEnd();
+
+// Backends
+$authBackend = new OCA\DAV\Connector\PublicAuth(\OC::$server->getConfig());
+
+$serverFactory = new OCA\DAV\Connector\Sabre\ServerFactory(
+	\OC::$server->getConfig(),
+	\OC::$server->getLogger(),
+	\OC::$server->getDatabaseConnection(),
+	\OC::$server->getUserSession(),
+	\OC::$server->getMountManager(),
+	\OC::$server->getTagManager()
+);
+
+$requestUri = \OC::$server->getRequest()->getRequestUri();
+
+$server = $serverFactory->createServer($baseuri, $requestUri, $authBackend, function () use ($authBackend) {
+	if (OCA\Files_Sharing\Helper::isOutgoingServer2serverShareEnabled() === false) {
+		// this is what is thrown when trying to access a non-existing share
+		throw new \Sabre\DAV\Exception\NotAuthenticated();
+	}
+
+	$share = $authBackend->getShare();
+	$rootShare = \OCP\Share::resolveReShare($share);
+	$owner = $rootShare['uid_owner'];
+	$isWritable = $share['permissions'] & (\OCP\Constants::PERMISSION_UPDATE | \OCP\Constants::PERMISSION_CREATE);
+	$fileId = $share['file_source'];
+
+	if (!$isWritable) {
+		\OC\Files\Filesystem::addStorageWrapper('readonly', function ($mountPoint, $storage) {
+			return new \OC\Files\Storage\Wrapper\PermissionsMask(array('storage' => $storage, 'mask' => \OCP\Constants::PERMISSION_READ + \OCP\Constants::PERMISSION_SHARE));
+		});
+	}
+
+	OC_Util::setupFS($owner);
+	$ownerView = \OC\Files\Filesystem::getView();
+	$path = $ownerView->getPath($fileId);
+
+	return new \OC\Files\View($ownerView->getAbsolutePath($path));
+});
+
+// And off we go!
+$server->exec();

apps/dav/lib/connector/sabre/serverfactory.php
@@ -1,0 +1,107 @@
+<?php
+/**
+ * @author Robin Appelman <icewind@owncloud.com>
+ *
+ * @copyright Copyright (c) 2015, ownCloud, Inc.
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace OCA\DAV\Connector\Sabre;
+
+use OCP\Files\Mount\IMountManager;
+use OCP\IConfig;
+use OCP\IDBConnection;
+use OCP\ILogger;
+use OCP\ITagManager;
+use OCP\IUserSession;
+use Sabre\DAV\Auth\Backend\BackendInterface;
+
+class ServerFactory {
+	public function __construct(
+		IConfig $config,
+		ILogger $logger,
+		IDBConnection $databaseConnection,
+		IUserSession $userSession,
+		IMountManager $mountManager,
+		ITagManager $tagManager
+	) {
+		$this->config = $config;
+		$this->logger = $logger;
+		$this->databaseConnection = $databaseConnection;
+		$this->userSession = $userSession;
+		$this->mountManager = $mountManager;
+		$this->tagManager = $tagManager;
+	}
+
+	/**
+	 * @param string $baseUri
+	 * @param string $requestUri
+	 * @param BackendInterface $authBackend
+	 * @param callable $viewCallBack callback that should return the view for the dav endpoint
+	 * @return Server
+	 */
+	public function createServer($baseUri, $requestUri, BackendInterface $authBackend, callable $viewCallBack) {
+		// Fire up server
+		$objectTree = new \OCA\DAV\Connector\Sabre\ObjectTree();
+		$server = new \OCA\DAV\Connector\Sabre\Server($objectTree);
+		// Set URL explicitly due to reverse-proxy situations
+		$server->httpRequest->setUrl($requestUri);
+		$server->setBaseUri($baseUri);
+
+		// Load plugins
+		$defaults = new \OC_Defaults();
+		$server->addPlugin(new \OCA\DAV\Connector\Sabre\BlockLegacyClientPlugin($this->config));
+		$server->addPlugin(new \Sabre\DAV\Auth\Plugin($authBackend, $defaults->getName()));
+		// FIXME: The following line is a workaround for legacy components relying on being able to send a GET to /
+		$server->addPlugin(new \OCA\DAV\Connector\Sabre\DummyGetResponsePlugin());
+		$server->addPlugin(new \OCA\DAV\Connector\Sabre\FilesPlugin($objectTree));
+		$server->addPlugin(new \OCA\DAV\Connector\Sabre\MaintenancePlugin($this->config));
+		$server->addPlugin(new \OCA\DAV\Connector\Sabre\ExceptionLoggerPlugin('webdav', $this->logger));
+
+		// wait with registering these until auth is handled and the filesystem is setup
+		$server->on('beforeMethod', function () use ($server, $objectTree, $viewCallBack) {
+			/** @var \OC\Files\View $view */
+			$view = $viewCallBack();
+			$rootInfo = $view->getFileInfo('');
+
+			// Create ownCloud Dir
+			if ($rootInfo->getType() === 'dir') {
+				$root = new \OCA\DAV\Connector\Sabre\Directory($view, $rootInfo);
+			} else {
+				$root = new \OCA\DAV\Connector\Sabre\File($view, $rootInfo);
+			}
+			$objectTree->init($root, $view, $this->mountManager);
+
+			$server->addPlugin(new \OCA\DAV\Connector\Sabre\QuotaPlugin($view));
+
+			if($this->userSession->isLoggedIn()) {
+				$server->addPlugin(new \OCA\DAV\Connector\Sabre\TagsPlugin($objectTree, $this->tagManager));
+				// custom properties plugin must be the last one
+				$server->addPlugin(
+					new \Sabre\DAV\PropertyStorage\Plugin(
+						new \OCA\DAV\Connector\Sabre\CustomPropertiesBackend(
+							$objectTree,
+							$this->databaseConnection,
+							$this->userSession->getUser()
+						)
+					)
+				);
+			}
+			$server->addPlugin(new \OCA\DAV\Connector\Sabre\CopyEtagHeaderPlugin());
+		}, 30); // priority 30: after auth (10) and acl(20), before lock(50) and handling the request
+		return $server;
+	}
+}

GitHubUser4234/server

apps/dav/appinfo/v1/publicwebdav.php
@@ -1,0 +1,74 @@
+<?php
+/**
+ * @author Joas Schilling <nickvergessen@owncloud.com>
+ * @author Morris Jobke <hey@morrisjobke.de>
+ * @author Robin Appelman <icewind@owncloud.com>
+ * @author Vincent Petry <pvince81@owncloud.com>
+ *
+ * @copyright Copyright (c) 2015, ownCloud, Inc.
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+// load needed apps
+$RUNTIME_APPTYPES = ['filesystem', 'authentication', 'logging'];
+
+OC_App::loadApps($RUNTIME_APPTYPES);
+
+OC_Util::obEnd();
+
+// Backends
+$authBackend = new OCA\DAV\Connector\PublicAuth(\OC::$server->getConfig());
+
+$serverFactory = new OCA\DAV\Connector\Sabre\ServerFactory(
+	\OC::$server->getConfig(),
+	\OC::$server->getLogger(),
+	\OC::$server->getDatabaseConnection(),
+	\OC::$server->getUserSession(),
+	\OC::$server->getMountManager(),
+	\OC::$server->getTagManager(),
+	\OC::$server->getEventDispatcher()
+);
+
+$requestUri = \OC::$server->getRequest()->getRequestUri();
+
+$server = $serverFactory->createServer($baseuri, $requestUri, $authBackend, function () use ($authBackend) {
+	$isAjax = (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest');
+	if (OCA\Files_Sharing\Helper::isOutgoingServer2serverShareEnabled() === false && !$isAjax) {
+		// this is what is thrown when trying to access a non-existing share
+		throw new \Sabre\DAV\Exception\NotAuthenticated();
+	}
+
+	$share = $authBackend->getShare();
+	$rootShare = \OCP\Share::resolveReShare($share);
+	$owner = $rootShare['uid_owner'];
+	$isWritable = $share['permissions'] & (\OCP\Constants::PERMISSION_UPDATE | \OCP\Constants::PERMISSION_CREATE);
+	$fileId = $share['file_source'];
+
+	if (!$isWritable) {
+		\OC\Files\Filesystem::addStorageWrapper('readonly', function ($mountPoint, $storage) {
+			return new \OC\Files\Storage\Wrapper\PermissionsMask(array('storage' => $storage, 'mask' => \OCP\Constants::PERMISSION_READ + \OCP\Constants::PERMISSION_SHARE));
+		});
+	}
+
+	OC_Util::setupFS($owner);
+	$ownerView = \OC\Files\Filesystem::getView();
+	$path = $ownerView->getPath($fileId);
+
+	return new \OC\Files\View($ownerView->getAbsolutePath($path));
+});
+
+// And off we go!
+$server->exec();

apps/dav/lib/connector/sabre/serverfactory.php
@@ -1,0 +1,113 @@
+<?php
+/**
+ * @author Joas Schilling <nickvergessen@owncloud.com>
+ * @author Robin Appelman <icewind@owncloud.com>
+ *
+ * @copyright Copyright (c) 2015, ownCloud, Inc.
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+namespace OCA\DAV\Connector\Sabre;
+
+use OCP\Files\Mount\IMountManager;
+use OCP\IConfig;
+use OCP\IDBConnection;
+use OCP\ILogger;
+use OCP\ITagManager;
+use OCP\IUserSession;
+use Sabre\DAV\Auth\Backend\BackendInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+class ServerFactory {
+	public function __construct(
+		IConfig $config,
+		ILogger $logger,
+		IDBConnection $databaseConnection,
+		IUserSession $userSession,
+		IMountManager $mountManager,
+		ITagManager $tagManager,
+		EventDispatcherInterface $dispatcher
+	) {
+		$this->config = $config;
+		$this->logger = $logger;
+		$this->databaseConnection = $databaseConnection;
+		$this->userSession = $userSession;
+		$this->mountManager = $mountManager;
+		$this->tagManager = $tagManager;
+		$this->dispatcher = $dispatcher;
+	}
+
+	/**
+	 * @param string $baseUri
+	 * @param string $requestUri
+	 * @param BackendInterface $authBackend
+	 * @param callable $viewCallBack callback that should return the view for the dav endpoint
+	 * @return Server
+	 */
+	public function createServer($baseUri, $requestUri, BackendInterface $authBackend, callable $viewCallBack) {
+		// Fire up server
+		$objectTree = new \OCA\DAV\Connector\Sabre\ObjectTree();
+		$server = new \OCA\DAV\Connector\Sabre\Server($objectTree);
+		// Set URL explicitly due to reverse-proxy situations
+		$server->httpRequest->setUrl($requestUri);
+		$server->setBaseUri($baseUri);
+
+		// Load plugins
+		$defaults = new \OC_Defaults();
+		$server->addPlugin(new \OCA\DAV\Connector\Sabre\MaintenancePlugin($this->config));
+		$server->addPlugin(new \OCA\DAV\Connector\Sabre\BlockLegacyClientPlugin($this->config));
+		$server->addPlugin(new \Sabre\DAV\Auth\Plugin($authBackend, $defaults->getName()));
+		// FIXME: The following line is a workaround for legacy components relying on being able to send a GET to /
+		$server->addPlugin(new \OCA\DAV\Connector\Sabre\DummyGetResponsePlugin());
+		$server->addPlugin(new \OCA\DAV\Connector\Sabre\ExceptionLoggerPlugin('webdav', $this->logger));
+		$server->addPlugin(new \OCA\DAV\Connector\Sabre\LockPlugin($objectTree));
+		$server->addPlugin(new \OCA\DAV\Connector\Sabre\ListenerPlugin($this->dispatcher));
+
+		// wait with registering these until auth is handled and the filesystem is setup
+		$server->on('beforeMethod', function () use ($server, $objectTree, $viewCallBack) {
+			/** @var \OC\Files\View $view */
+			$view = $viewCallBack();
+			$rootInfo = $view->getFileInfo('');
+
+			// Create ownCloud Dir
+			if ($rootInfo->getType() === 'dir') {
+				$root = new \OCA\DAV\Connector\Sabre\Directory($view, $rootInfo);
+			} else {
+				$root = new \OCA\DAV\Connector\Sabre\File($view, $rootInfo);
+			}
+			$objectTree->init($root, $view, $this->mountManager);
+
+			$server->addPlugin(new \OCA\DAV\Connector\Sabre\FilesPlugin($objectTree, $view));
+			$server->addPlugin(new \OCA\DAV\Connector\Sabre\QuotaPlugin($view));
+
+			if($this->userSession->isLoggedIn()) {
+				$server->addPlugin(new \OCA\DAV\Connector\Sabre\TagsPlugin($objectTree, $this->tagManager));
+				// custom properties plugin must be the last one
+				$server->addPlugin(
+					new \Sabre\DAV\PropertyStorage\Plugin(
+						new \OCA\DAV\Connector\Sabre\CustomPropertiesBackend(
+							$objectTree,
+							$this->databaseConnection,
+							$this->userSession->getUser()
+						)
+					)
+				);
+			}
+			$server->addPlugin(new \OCA\DAV\Connector\Sabre\CopyEtagHeaderPlugin());
+		}, 30); // priority 30: after auth (10) and acl(20), before lock(50) and handling the request
+		return $server;
+	}
+}