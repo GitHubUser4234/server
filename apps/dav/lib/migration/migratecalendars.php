vincchan/core

Migration of calendars

apps/dav/lib/migration/migratecalendars.php
@@ -1,0 +1,95 @@
+<?php
+
+namespace OCA\Dav\Migration;
+
+use OCA\DAV\CalDAV\CalDavBackend;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MigrateCalendars {
+
+	/** @var CalendarAdapter */
+	protected $adapter;
+
+	/** @var CalDavBackend */
+	private $backend;
+
+	/**
+	 * @param CalendarAdapter $adapter
+	 * @param CalDavBackend $backend
+	 */
+	function __construct(CalendarAdapter $adapter,
+						 CalDavBackend $backend
+	) {
+		$this->adapter = $adapter;
+		$this->backend = $backend;
+	}
+
+	/**
+	 * @param string $user
+	 */
+	public function migrateForUser($user) {
+
+		$this->adapter->foreachCalendar($user, function($calendar) use ($user) {
+			$principal = "principals/users/$user";
+			$calendarByUri = $this->backend->getCalendarByUri($principal, $calendar['uri']);
+			if (!is_null($calendarByUri)) {
+				return;
+			}
+
+			$newId = $this->backend->createCalendar($principal, $calendar['uri'], [
+				'{DAV:}displayname' => $calendar['displayname'],
+				'{urn:ietf:params:xml:ns:caldav}calendar-description' => $calendar['displayname'],
+				'{urn:ietf:params:xml:ns:caldav}calendar-timezone'    => $calendar['timezone'],
+				'{http://apple.com/ns/ical/}calendar-order'  => $calendar['calendarorder'],
+				'{http://apple.com/ns/ical/}calendar-color'  => $calendar['calendarcolor'],
+			]);
+
+			$this->migrateCalendar($calendar['id'], $newId);
+			$this->migrateShares($calendar['id'], $newId);
+		});
+	}
+
+	public function setup() {
+		$this->adapter->setup();
+	}
+
+	/**
+	 * @param int $calendarId
+	 * @param int $newCalendarId
+	 */
+	private function migrateCalendar($calendarId, $newCalendarId) {
+		$this->adapter->foreachCalendarObject($calendarId, function($calObject) use ($newCalendarId) {
+			$this->backend->createCalendarObject($newCalendarId, $calObject['uri'], $calObject['carddata']);
+		});
+	}
+
+	/**
+	 * @param int $calendarId
+	 * @param int $newCalendarId
+	 */
+	private function migrateShares($calendarId, $newCalendarId) {
+		$shares =$this->adapter->getShares($calendarId);
+		if (empty($shares)) {
+			return;
+		}
+
+		$add = array_map(function($s) {
+			$prefix = 'principal:principals/users/';
+			if ($s['share_type'] === 1) {
+				$prefix = 'principal:principals/groups/';
+			}
+			return [
+				'href' => $prefix . $s['share_with']
+			];
+		}, $shares);
+
+		/*
+		$newAddressBook = $this->backend->getCalendarById($newCalendarId);
+		$book = new AddressBook($this->backend, $newAddressBook);
+		$this->backend->updateShares($book, $add, []);
+		*/
+	}
+}

apps/dav/tests/unit/migration/migratecalendartest.php
@@ -1,0 +1,76 @@
+<?php
+/**
+ * @author Thomas Müller <thomas.mueller@tmit.eu>
+ *
+ * @copyright Copyright (c) 2016, ownCloud, Inc.
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+namespace OCA\DAV\Tests\Unit\Migration;
+
+use OCA\DAV\CalDAV\CalDavBackend;
+use OCA\Dav\Migration\CalendarAdapter;
+use Test\TestCase;
+
+class MigrateCalendarTest extends TestCase {
+
+	public function testMigration() {
+		/** @var CalendarAdapter | \PHPUnit_Framework_MockObject_MockObject $adapter */
+		$adapter = $this->mockAdapter();
+
+		/** @var CalDavBackend | \PHPUnit_Framework_MockObject_MockObject $cardDav */
+		$cardDav = $this->getMockBuilder('\OCA\Dav\CalDAV\CalDAVBackend')->disableOriginalConstructor()->getMock();
+		$cardDav->method('createCalendar')->willReturn(666);
+		$cardDav->expects($this->once())->method('createCalendar')->with('principals/users/test01', 'test_contacts');
+		$cardDav->expects($this->once())->method('createCalendarObject')->with(666, '63f0dd6c-39d5-44be-9d34-34e7a7441fc2.ics', 'BEGIN:VCARD');
+
+		$m = new \OCA\Dav\Migration\MigrateCalendars($adapter, $cardDav);
+		$m->migrateForUser('test01');
+	}
+
+	/**
+	 * @return \PHPUnit_Framework_MockObject_MockObject
+	 */
+	private function mockAdapter($shares = []) {
+		$adapter = $this->getMockBuilder('\OCA\Dav\Migration\CalendarAdapter')
+			->disableOriginalConstructor()
+			->getMock();
+		$adapter->method('foreachCalendar')->willReturnCallback(function ($user, \Closure $callBack) {
+			$callBack([
+				// calendarorder | calendarcolor | timezone | components
+				'id' => 0,
+				'userid' => $user,
+				'displayname' => 'Test Contacts',
+				'uri' => 'test_contacts',
+				'ctag' => 1234567890,
+				'active' => 1,
+				'calendarorder' => '0',
+				'calendarcolor' => '#b3dc6c',
+				'timezone' => null,
+				'components' => 'VEVENT,VTODO,VJOURNAL'
+			]);
+		});
+		$adapter->method('foreachCalendarObject')->willReturnCallback(function ($addressBookId, \Closure $callBack) {
+			$callBack([
+				'userid' => $addressBookId,
+				'uri' => '63f0dd6c-39d5-44be-9d34-34e7a7441fc2.ics',
+				'carddata' => 'BEGIN:VCARD'
+			]);
+		});
+		$adapter->method('getShares')->willReturn($shares);
+		return $adapter;
+	}
+
+}

GitHubUser4234/server

apps/dav/lib/migration/migratecalendars.php
@@ -1,0 +1,94 @@
+<?php
+
+namespace OCA\Dav\Migration;
+
+use OCA\DAV\CalDAV\CalDavBackend;
+use OCA\DAV\CalDAV\Calendar;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MigrateCalendars {
+
+	/** @var CalendarAdapter */
+	protected $adapter;
+
+	/** @var CalDavBackend */
+	private $backend;
+
+	/**
+	 * @param CalendarAdapter $adapter
+	 * @param CalDavBackend $backend
+	 */
+	function __construct(CalendarAdapter $adapter,
+						 CalDavBackend $backend
+	) {
+		$this->adapter = $adapter;
+		$this->backend = $backend;
+	}
+
+	/**
+	 * @param string $user
+	 */
+	public function migrateForUser($user) {
+
+		$this->adapter->foreachCalendar($user, function($calendar) use ($user) {
+			$principal = "principals/users/$user";
+			$calendarByUri = $this->backend->getCalendarByUri($principal, $calendar['uri']);
+			if (!is_null($calendarByUri)) {
+				return;
+			}
+
+			$newId = $this->backend->createCalendar($principal, $calendar['uri'], [
+				'{DAV:}displayname' => $calendar['displayname'],
+				'{urn:ietf:params:xml:ns:caldav}calendar-description' => $calendar['displayname'],
+				'{urn:ietf:params:xml:ns:caldav}calendar-timezone'    => $calendar['timezone'],
+				'{http://apple.com/ns/ical/}calendar-order'  => $calendar['calendarorder'],
+				'{http://apple.com/ns/ical/}calendar-color'  => $calendar['calendarcolor'],
+			]);
+
+			$this->migrateCalendar($calendar['id'], $newId);
+			$this->migrateShares($calendar['id'], $newId);
+		});
+	}
+
+	public function setup() {
+		$this->adapter->setup();
+	}
+
+	/**
+	 * @param int $calendarId
+	 * @param int $newCalendarId
+	 */
+	private function migrateCalendar($calendarId, $newCalendarId) {
+		$this->adapter->foreachCalendarObject($calendarId, function($calObject) use ($newCalendarId) {
+			$this->backend->createCalendarObject($newCalendarId, $calObject['uri'], $calObject['calendardata']);
+		});
+	}
+
+	/**
+	 * @param int $calendarId
+	 * @param int $newCalendarId
+	 */
+	private function migrateShares($calendarId, $newCalendarId) {
+		$shares =$this->adapter->getShares($calendarId);
+		if (empty($shares)) {
+			return;
+		}
+
+		$add = array_map(function($s) {
+			$prefix = 'principal:principals/users/';
+			if ($s['share_type'] === 1) {
+				$prefix = 'principal:principals/groups/';
+			}
+			return [
+				'href' => $prefix . $s['share_with']
+			];
+		}, $shares);
+
+		$newCalendar = $this->backend->getCalendarById($newCalendarId);
+		$calendar = new Calendar($this->backend, $newCalendar);
+		$this->backend->updateShares($calendar, $add, []);
+	}
+}

apps/dav/tests/unit/migration/migratecalendartest.php
@@ -1,0 +1,76 @@
+<?php
+/**
+ * @author Thomas Müller <thomas.mueller@tmit.eu>
+ *
+ * @copyright Copyright (c) 2016, ownCloud, Inc.
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+namespace OCA\DAV\Tests\Unit\Migration;
+
+use OCA\DAV\CalDAV\CalDavBackend;
+use OCA\Dav\Migration\CalendarAdapter;
+use Test\TestCase;
+
+class MigrateCalendarTest extends TestCase {
+
+	public function testMigration() {
+		/** @var CalendarAdapter | \PHPUnit_Framework_MockObject_MockObject $adapter */
+		$adapter = $this->mockAdapter();
+
+		/** @var CalDavBackend | \PHPUnit_Framework_MockObject_MockObject $cardDav */
+		$cardDav = $this->getMockBuilder('\OCA\Dav\CalDAV\CalDAVBackend')->disableOriginalConstructor()->getMock();
+		$cardDav->method('createCalendar')->willReturn(666);
+		$cardDav->expects($this->once())->method('createCalendar')->with('principals/users/test01', 'test_contacts');
+		$cardDav->expects($this->once())->method('createCalendarObject')->with(666, '63f0dd6c-39d5-44be-9d34-34e7a7441fc2.ics', 'BEGIN:VCARD');
+
+		$m = new \OCA\Dav\Migration\MigrateCalendars($adapter, $cardDav);
+		$m->migrateForUser('test01');
+	}
+
+	/**
+	 * @return \PHPUnit_Framework_MockObject_MockObject
+	 */
+	private function mockAdapter($shares = []) {
+		$adapter = $this->getMockBuilder('\OCA\Dav\Migration\CalendarAdapter')
+			->disableOriginalConstructor()
+			->getMock();
+		$adapter->method('foreachCalendar')->willReturnCallback(function ($user, \Closure $callBack) {
+			$callBack([
+				// calendarorder | calendarcolor | timezone | components
+				'id' => 0,
+				'userid' => $user,
+				'displayname' => 'Test Contacts',
+				'uri' => 'test_contacts',
+				'ctag' => 1234567890,
+				'active' => 1,
+				'calendarorder' => '0',
+				'calendarcolor' => '#b3dc6c',
+				'timezone' => null,
+				'components' => 'VEVENT,VTODO,VJOURNAL'
+			]);
+		});
+		$adapter->method('foreachCalendarObject')->willReturnCallback(function ($addressBookId, \Closure $callBack) {
+			$callBack([
+				'userid' => $addressBookId,
+				'uri' => '63f0dd6c-39d5-44be-9d34-34e7a7441fc2.ics',
+				'calendardata' => 'BEGIN:VCARD'
+			]);
+		});
+		$adapter->method('getShares')->willReturn($shares);
+		return $adapter;
+	}
+
+}